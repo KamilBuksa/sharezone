53a4136335d2efd79e4e352d31a30ac202ef5f89
Kamil <buksakowskikamil@gmail.com>
2022-01-27 22:49:29 +0100

    configurate docker and TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule,
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass123',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,7 +1,17 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
 export class Article {
+    @PrimaryGeneratedColumn()
     id:number;
+
+    @Column()
     title:string;
+
+    @Column()
     lead:string;
+
+    @Column()
     body:string;
     // createdAt: Date;
 }
