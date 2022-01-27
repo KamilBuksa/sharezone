9e5d16c494e9cf0aaa9d206869a451e9dc4c5d0f
Kamil <buksakowskikamil@gmail.com>
2022-01-27 16:04:35 +0100

    add findArticles, findOneArticle methods

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,10 +1,31 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 
 @Controller('articles')
 export class ArticlesController {
 
     @Get()
-    addArticle(){
-        return 'article added'
+    findAllArticles(){
+        return `find all`
     }
+
+    @Get(':id')
+    findOneArticle(@Param('id') id:number){
+        return `finded article ${id}`
+    }
+
+    @Post()
+    createArticle(@Body() body){
+        return body
+    }
+
+    @Patch(':id')
+    updateArticle(@Param('id') id){
+        return `update article ${id}`
+    }
+
+    @Delete(':id')
+    deleteArticle(@Param('id') id){
+        return `deleted article ${id}`
+    }
+
 }
